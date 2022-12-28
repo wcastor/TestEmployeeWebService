f9dba19f614e65dc21df70183a739308066d1849
Alexey <idontwanttosharemyemail@mail.ru>
2022-12-28 16:15:13 +0300

    added some logic

--- src/main/scala/employeeservice/logic/httpendpoint/EmployeeHttpEndpoint.scala
+++ src/main/scala/employeeservice/logic/httpendpoint/EmployeeHttpEndpoint.scala
@@ -6,7 +6,7 @@ import employeeservice.logic.db.EmployeeRepoImpl
 import io.circe.generic.semiauto._
 import io.circe.syntax.EncoderOps
 import io.circe.{ Decoder, Encoder }
-import org.http4s.HttpService
+import org.http4s.{ HttpService, Response }
 import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
 
@@ -15,12 +15,26 @@ class EmployeeHttpEndpoint(dao: EmployeeRepoImpl) extends Http4sDsl[IO] {
   implicit val decodeEmployee: Decoder[Employee] = deriveDecoder[Employee]
   implicit val encodeEmployee: Encoder[Employee] = deriveEncoder[Employee]
 
+  val apiVersion = "Employees Service. Version 0.1"
+
   val service: HttpService[IO] = HttpService[IO] {
     case GET -> Root / "employees" =>
       for {
         result   <- dao.getAll()
         response <- Ok(result.asJson)
       } yield response
+    case GET -> Root / "ping" => Ok(apiVersion.asJson)
+    case req @ POST -> Root / "employee" =>
+      for {
+        employee <- req.decodeJson[Employee]
+        response <- dao.create(employee)
+        result   <- check(response)
+      } yield result
+  }
+
+  def check(res: Either[String, Employee]): IO[Response[IO]] = res match {
+    case Left(e)  => InternalServerError(e.toString)
+    case Right(r) => Ok(r.asJson)
   }
 
 }
